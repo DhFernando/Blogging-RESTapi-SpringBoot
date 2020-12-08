db6ca32164af298bf9ec3729bca59798c3bd90e3
dhfernando <hdilshan.fernando11@gmail.com>
2020-12-09 01:08:29 +0530

    develop error handeling for user

--- src/main/java/com/dh/demo/Controllers/UserController.java
+++ src/main/java/com/dh/demo/Controllers/UserController.java
@@ -1,20 +1,21 @@
 package com.dh.demo.Controllers;
 
-import com.dh.demo.DTO.Article.ArticleDTO;
 import com.dh.demo.DTO.User.UserCreateDto;
 import com.dh.demo.DTO.User.UserDto;
+import com.dh.demo.Error.ApiError;
 import com.dh.demo.Models.Article;
 import com.dh.demo.Models.User;
 import com.dh.demo.Projection.UserProjection;
 import com.dh.demo.services.ArticleService;
 import com.dh.demo.services.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
 
 @RestController
 public class UserController {
@@ -30,6 +31,11 @@ public class UserController {
     public List<Article> getArticlesByUserId(@PathVariable Integer id){
         return articleService.getArticlesByUserId(id);
     }
+    @GetMapping("user/{id}")
+    public UserDto getUser(@PathVariable Integer id){
+         User u = service.get(id);
+         return  new UserDto(u);
+    }
 
     @PostMapping("/user")
     public UserDto createUser(@RequestBody UserCreateDto userCreateDto ){
@@ -38,6 +44,13 @@ public class UserController {
                 "user" , new Date() , userCreateDto.getImageUrl()  );
         User createdUser = service.save(u);
         return new UserDto(createdUser);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNoSuchElementException(NoSuchElementException exception , HttpServletRequest request){
+        ApiError error = new ApiError(404 , "user not found" , new Date().getTime() , request.getServletPath());
+        return error;
     }
 
 }
